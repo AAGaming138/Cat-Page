6e928e587a92a2ee273fa6e1a1ed32c4f2f86fcb
“AAGaming138” <aagaming138@gmail.com>
2023-08-02 15:23:09 +1000

    Some fixes to backswing + other stuff

---
+++ Scripts/talents.py
@@ -0,0 +1,6 @@
+talents = {
+    (1, True, True, False): ": Increases weaken duration by{}{}<sup>{}s</sup> ",
+    (1, False, True, False): ": Adds a {}% chance to weaken enemies to {}% for{}{}<sup>{}s</sup> ",
+    (1, True, False, True): ": Upgrades chance to weaken enemies by{}{}",
+    (1, False, False, True): ": Adds a {}% chance to weaken enemies to {}% for {}, improves by {}%{}",
+}
